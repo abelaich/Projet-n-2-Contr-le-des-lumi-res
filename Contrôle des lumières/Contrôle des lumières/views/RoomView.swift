c192e164e393619589d7293441d41ec195e46caa
Alae Belaich <belaich.alae@gmail.com>
2024-12-02 09:58:22 +0100

    Add RoomView

---
+++ Contrôle des lumières/Contrôle des lumières/views/RoomView.swift
@@ -0,0 +1,31 @@
+//
+//  RoomView.swift
+//  Contrôle des lumières
+//
+//  Created by Alae on 02/12/2024.
+//
+
+import SwiftUI
+
+struct RoomView: View {
+    @Binding var room: Room // Lier la pièce à cette vue
+
+    var body: some View {
+        HStack(spacing: 16) {
+            Text(room.name)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            // Affichage de l'icône de la lampe selon l'état de l'éclairage
+            Image(systemName: room.lightState == .on ? "lightbulb.fill" : room.lightState == .off ? "lightbulb" : "lightbulb.slash")
+                .foregroundColor(room.lightState == .on ? .yellow : .gray)
+
+            // Slider pour ajuster le niveau d'éclairage, désactivé si la pièce est défectueuse
+            Slider(value: $room.lightLevel, in: 0...100, step: 1)
+                .disabled(room.outOfOrder) // Désactiver si la pièce est en panne
+
+            Text("\(room.lightLevel)%")
+        }
+        .padding()
+    }
+}
+
