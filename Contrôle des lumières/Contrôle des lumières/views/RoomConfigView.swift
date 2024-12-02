aba77ee991628e73f399db1c9edea0c36d09f194
Alae Belaich <belaich.alae@gmail.com>
2024-12-02 10:03:09 +0100

    Add RoomConfigView

--- Contrôle des lumières/Contrôle des lumières/views/RoomConfigView.swift
+++ Contrôle des lumières/Contrôle des lumières/views/RoomConfigView.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct RoomConfigView: View {
+    @Binding var room: Room
+
+    var body: some View {
+        Form {
+            Picker("Light Type", selection: $room.lightType) {
+                ForEach(LightType.allCases, id: \.self) {
+                    Text($0.rawValue)
+                }
+            }
+            Stepper("Light Power (lumens): \(room.lightLevel)", value: $room.lightLevel, in: 0...1000, step: 10)
+            Toggle("Out of Order", isOn: $room.outOfOrder)
+            Button("Copy to Clipboard") {
+                UIPasteboard.general.string = "\(room.name): \(room.lightType.rawValue), \(room.lightLevel) lumens"
+            }
+        }
+        .navigationTitle(room.name)
+    }
+}
