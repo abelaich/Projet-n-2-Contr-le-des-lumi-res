e064fbff172536d7f0479a552f1d6b031fbdd6ed
Alae Belaich <belaich.alae@gmail.com>
2024-12-02 09:34:08 +0100

    Projet SwiftUI LightControl

---
+++ Contrôle des lumières/Contrôle des lumières/Contro_le_des_lumie_resApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contro_le_des_lumie_resApp.swift
+//  Contrôle des lumières
+//
+//  Created by Alae on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Contro_le_des_lumie_resApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
